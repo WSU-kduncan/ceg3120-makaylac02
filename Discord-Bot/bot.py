ae5f6a3965a24e1de87cce7f3e20a50f4ff14407
Makayla Carr <carr.137@wright.edu>
2022-01-21 05:22:31 +0000

    added bot code and .env. Bot is running at this point.

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -1,4 +1,4 @@
-mport os
+import os
 
 import discord
 import random
@@ -13,41 +13,41 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-        print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected to Discord!')
 
-            for guild in client.guilds:
-                        if guild.name == GUILD:
-                                        break
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-                                        print(
-                                                        f'{client.user} is connected to the following guild:\n'
-                                                                f'{guild.name}(id: {guild.id})'
-                                                                    )
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
-                                        @client.event
-                                        async def on_message(message):
-                                                if message.author == client.user:
-                                                            return
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-                                                            brooklyn_99_quotes = [
-                                                                            'I\'m the human form of the 💯 emoji.',
-                                                                                    'Bingpot!',
-                                                                                            (
-                                                                                                            'Cool. Cool cool cool cool cool cool cool, '
-                                                                                                                        'no doubt no doubt no doubt no doubt.'
-                                                                                                                                ),
-                                                                                                ]
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-                                                                hitchhiker_quotes = [
-                                                                                'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-                                                                                        'It is a mistake to think you can solve any major problems just with potatoes.',
-                                                                                                'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-                                                                                                        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
-                                                                                                            ]
+    hitchhiker_quotes = [
+        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
+        'It is a mistake to think you can solve any major problems just with potatoes.',
+        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
+        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+    ]
 
-                                                                    if message.content == 'towel!':
-                                                                                #response = random.choice(brooklyn_99_quotes)
-                                                                                        response = random.choice(hitchhiker_quotes)
-                                                                                                await message.channel.send(response)
+    if message.content == 'towel!':
+        #response = random.choice(brooklyn_99_quotes)
+        response = random.choice(hitchhiker_quotes)
+        await message.channel.send(response)
 
-                                                                                                client.run(TOKEN)
+client.run(TOKEN)
